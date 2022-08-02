2b8f91682ee27c1c9d47bd23f85c991bb6a9f848
Aaron Noe <aarminoe@gmail.com>
2022-08-01 20:15:18 -0500

    fixed groups table to work with users many to many relationship

--- db/migrate/20220726000811_create_groups.rb
+++ db/migrate/20220726000811_create_groups.rb
@@ -3,7 +3,6 @@ class CreateGroups < ActiveRecord::Migration[6.1]
     create_table :groups do |t|
       t.string :name 
       t.integer :number_of_members
-      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_30_020754) do
+ActiveRecord::Schema.define(version: 2022_07_31_134437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 2022_07_30_020754) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.integer "number_of_members"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -64,6 +63,13 @@ ActiveRecord::Schema.define(version: 2022_07_30_020754) do
   create_table "posts", force: :cascade do |t|
     t.string "post"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "usergroups", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
