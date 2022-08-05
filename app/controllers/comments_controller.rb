e6646becbf4b5d6a913168fcaa653f32bd16af51
Aaron Noe <aarminoe@gmail.com>
2022-08-05 18:01:28 -0500

    users can now comment on any post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,17 @@
 class CommentsController < ApplicationController
     
+    def create
+        comment = Comment.create(comment_params)
+        if comment.valid?
+            render json: comment, status: :created 
+        else
+            render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def comment_params
+        params.permit(:post_id, :comment)
+    end
 end
--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -4,16 +4,37 @@ import React, { useState } from "react"
 function Comments({post, user}) {
 
     const [addedComment, setAddedComment] = useState('')
+    const [seeComments, setSeeComments] = useState(false)
+
+    function handleAddComment(e) {
+        e.preventDefault()
+        console.log(post)
+        fetch(`/users/${post.user.id}/posts/${post.id}/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                comment: addedComment,
+                post_id: post.id
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }
 
     return (
+        
         <div className="comments-card">
-            <form>
-                <input type='text'></input>
+            <button onClick={() => setSeeComments((seeComments) => !seeComments)}>See Comments</button>
+            {seeComments ? <form onSubmit={handleAddComment}>
+                <input type='text' onChange={(e) => setAddedComment(e.target.value)}></input>
                 <button>Add Comment</button>
-            </form>
-            {post.comments ? post.comments.map((comment) => {
+            </form> 
+            : null}
+            {seeComments ? post.comments.map((comment) => {
                 return <Comment comment={comment} post={post} />
-            }):null}
+            }): null}
         </div>
     )
 }
